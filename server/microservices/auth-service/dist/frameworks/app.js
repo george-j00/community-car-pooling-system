5f0e944ba8b850b74b7dbc2b1ee720a13302588b
GeorgeJose <subingeorge027@gmail.com>
2024-02-19 23:32:18 +0530

    Fix : changed framework layer

---
+++ server/microservices/auth-service/dist/frameworks/app.js
@@ -0,0 +1,27 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const dotenv_1 = __importDefault(require("dotenv"));
+const cors_1 = __importDefault(require("cors"));
+const cookie_parser_1 = __importDefault(require("cookie-parser"));
+const simple_auth_connection_1 = require("simple-auth-connection");
+dotenv_1.default.config();
+const app = (0, express_1.default)();
+const port = process.env.PORT;
+(0, simple_auth_connection_1.start)(process.env.MONGODB_URI);
+app.use((0, cookie_parser_1.default)());
+app.use((0, cors_1.default)());
+app.use((req, res, next) => {
+    res.setHeader('Cache-Control', 'no-cache, no-store, must-revalidate');
+    res.setHeader('Pragma', 'no-cache');
+    res.setHeader('Expires', '0');
+    next();
+});
+app.use(express_1.default.json());
+app.use(express_1.default.urlencoded({ extended: false }));
+app.listen(port, () => {
+    console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
+});
---
+++ server/microservices/auth-service/src/frameworks/app.ts
@@ -0,0 +1,28 @@
+import express, { Express, Request, Response } from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import { start } from 'simple-auth-connection';
+
+dotenv.config();
+const app : Express = express();
+const port = process.env.PORT;
+
+start(process.env.MONGODB_URI!)
+app.use(cookieParser());
+app.use(cors());
+
+app.use((req, res, next) => {
+  res.setHeader('Cache-Control', 'no-cache, no-store, must-revalidate');
+  res.setHeader('Pragma', 'no-cache');
+  res.setHeader('Expires', '0');
+  next();
+});
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+
+app.listen(port, () => {
+  console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
+});    
